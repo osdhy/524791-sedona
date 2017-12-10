a29e22b46014f23ff02b6dc64190416748769cf0
Pavel Osadchiy <hellofromosdhy@gmail.com>
2017-12-10 21:59:52 +0300

    Исправления для повторной проверки

--- js/script.js
+++
@@ -1,7 +0,0 @@
-(function() {
-    let button = document.querySelector('.expand-button');
-    let form = document.querySelector('.form-container');
-    button.addEventListener('click', ()=>{
-      form.style.display = form.style.display === 'none' ? '' : 'none'
-    })
-  })()
